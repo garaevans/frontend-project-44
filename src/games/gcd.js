7cdd6a857a9286d14ab913437f11a8183180e46a
Natalia Garaeva <garaevans@gmail.com>
2023-04-10 18:29:49 +0500

    Update gcd.js

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,46 +1,28 @@
-import readlineSync from 'readline-sync';
 import generateRandomNum from '../utils.js';
-import { greetPlayer, rules, maxNumberOfRounds } from '../index.js';
+import startGame from '../index.js';
 
-const gcdGame = () => {
-  const playerName = greetPlayer();
-
-  console.log(rules.gcd);
-
-  const getGCD = (num1, num2) => {
-    let a = num1;
-    let b = num2;
-    while (a !== b) {
-      if (a > b) {
-        a -= b;
-      } else {
-        b -= a;
-      }
-    }
-
-    return a;
-  };
-
-  const startRound = () => {
-    const randomNum1 = generateRandomNum(1, 25);
-    const randomNum2 = generateRandomNum(1, 25);
-    console.log(`Question: ${randomNum1} ${randomNum2}`);
-    const playerAnswer = readlineSync.question('Your answer: ');
-    const correctAnswer = getGCD(randomNum1, randomNum2).toString();
-    return [playerAnswer, correctAnswer];
-  };
-
-  for (let i = 0; i < maxNumberOfRounds; i += 1) {
-    const [playerAnswer, correctAnswer] = startRound();
-    if (playerAnswer === correctAnswer) {
-      console.log('Correct!');
+const getGCD = (num1, num2) => {
+  let a = num1;
+  let b = num2;
+  while (a !== b) {
+    if (a > b) {
+      a -= b;
     } else {
-      console.log(`'${playerAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${playerName}!`);
-      return;
+      b -= a;
     }
   }
 
-  console.log(`Congratulations, ${playerName}!`);
+  return a;
 };
+
+const startRound = () => {
+  const randomNum1 = generateRandomNum(1, 25);
+  const randomNum2 = generateRandomNum(1, 25);
+  const question = `${randomNum1} ${randomNum2}`;
+  const correctAnswer = getGCD(randomNum1, randomNum2).toString();
+  return [question, correctAnswer];
+};
+
+const gcdGame = () => startGame('gcd', startRound);
 
 export default gcdGame;
