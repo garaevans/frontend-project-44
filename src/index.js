9afbc3e95fb7dc9e4c016e35a6e01269d275c5e4
Natalia Garaeva <garaevans@gmail.com>
2023-04-13 01:42:36 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,42 +1,29 @@
 import readlineSync from 'readline-sync';
 
-const greetPlayer = () => {
+const roundsCount = 3;
+
+const runGame = (rule, generateRound) => {
   console.log('Welcome to the Brain Games!');
   const playerName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${playerName}!`);
-  return playerName;
-};
 
-const rules = {
-  even: 'Answer "yes" if the number is even, otherwise answer "no".',
-  calc: 'What is the result of the expression?',
-  gcd: 'Find the greatest common divisor of given numbers.',
-  progression: 'What number is missing in the progression?',
-  prime: 'Answer "yes" if given number is prime. Otherwise answer "no".',
-};
+  console.log(rule);
 
-const maxNumberOfRounds = 3;
-
-const startGame = (gameName, startRound) => {
-  const playerName = greetPlayer();
-
-  console.log(rules[gameName]);
-
-  for (let i = 0; i < maxNumberOfRounds; i += 1) {
-    const [question, correctAnswer] = startRound();
+  for (let i = 0; i < roundsCount; i += 1) {
+    const [question, answer] = generateRound();
     console.log(`Question: ${question}`);
     const playerAnswer = readlineSync.question('Your answer: ');
 
-    if (playerAnswer === correctAnswer) {
-      console.log('Correct!');
-    } else {
-      console.log(`'${playerAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+    if (playerAnswer !== answer) {
+      console.log(`'${playerAnswer}' is wrong answer ;(. Correct answer was '${answer}'.`);
       console.log(`Let's try again, ${playerName}!`);
       return;
     }
+
+    console.log('Correct!');
   }
 
   console.log(`Congratulations, ${playerName}!`);
 };
 
-export default startGame;
+export default runGame;
