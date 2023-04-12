a3c418e0a1becde4f5af791bca0b4504fa34208e
Natalia Garaeva <garaevans@gmail.com>
2023-04-13 01:42:12 +0500

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,3 @@
-const generateRandomNum = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+const generateRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-export default generateRandomNum;
+export default generateRandomNumber;
