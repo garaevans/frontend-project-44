09953c6b5ae88f2d262f0cffcb221a3940752f71
Natalia Garaeva <garaevans@gmail.com>
2023-04-13 01:57:08 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 
 const roundsCount = 3;
 
-const runGame = (rule, generateRound) => {
+const startGame = (rule, generateRound) => {
   console.log('Welcome to the Brain Games!');
   const playerName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${playerName}!`);
@@ -26,4 +26,4 @@ const runGame = (rule, generateRound) => {
   console.log(`Congratulations, ${playerName}!`);
 };
 
-export default runGame;
+export default startGame;
